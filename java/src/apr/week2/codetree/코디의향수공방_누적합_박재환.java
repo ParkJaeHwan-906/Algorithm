3d43967f54d9828bfe7f50c57ae0c9a4502e4af6
Hwannee00 <doormoo2@naver.com>
2026-04-13 15:58:52 +0900

    CODETREE 코디의 향수 공방 풀이(누적합) - 박재환

---
+++ java/src/apr/week2/codetree/코디의향수공방_누적합_박재환.java
@@ -0,0 +1,144 @@
+package apr.week2.codetree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 코디의향수공방_누적합_박재환 {
+    static BufferedReader br;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        init();
+        br.close();
+        System.out.println(sb);
+    }
+    static final int SET = 1;
+    static final int ADD = 2;
+    static final int DEL = 3;
+    static final int QUERY_1 = 4;
+    static final int QUERY_2 = 5;
+
+    static StringTokenizer st;
+    static void init() throws IOException {
+        int q = Integer.parseInt(br.readLine().trim());
+
+        while(q-- > 0) {
+            st = new StringTokenizer(br.readLine().trim());
+            int cmd = Integer.parseInt(st.nextToken());
+
+            if(cmd == SET) { set(); }
+            else if(cmd == ADD) { add(); }
+            else if(cmd == DEL) {
+                int result = del();
+                sb.append(result).append('\n');
+            }
+            else if(cmd == QUERY_1) {
+                int result = query1();
+                sb.append(result).append('\n');
+            }
+            else if(cmd == QUERY_2) {
+                long result = query2();
+                sb.append(result).append('\n');
+            }
+        }
+    }
+
+    static class Perfume {
+        int power;
+        boolean removed;
+
+        Perfume(int power) {
+            this.power = power;
+            this.removed = false;
+        }
+    }
+    static int n;
+    static List<Perfume> perfumes;
+    static void set() {
+        perfumes = new ArrayList<>();
+        n = Integer.parseInt(st.nextToken());
+        // 1 - based 맞추기
+        perfumes.add(new Perfume(-1));
+        for(int i = 0; i < n; i++) {
+            int power = Integer.parseInt(st.nextToken());
+            Perfume p = new Perfume(power);
+            perfumes.add(p);
+        }
+    }
+    static void add() {
+        int power = Integer.parseInt(st.nextToken());
+        Perfume p = new Perfume(power);
+        perfumes.add(p);
+        n++;
+    }
+    static int del() {
+        int id = Integer.parseInt(st.nextToken());
+        // 존재하지 않거나, 이미 폐기된 향수
+        if(id > n || perfumes.get(id).removed) return -1;
+        Perfume p = perfumes.get(id);
+        p.removed = true;
+        return p.power;
+    }
+    static int query1() {
+        int k = Integer.parseInt(st.nextToken());
+        /**
+         * 정확히 k 가 되도록 향료를 선택
+         * 필요한 형료의 최소 개수 출력
+         * 같은 번호의 향료 여러번 사용 가능
+         */
+        int[] dp = new int[k + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for(int i = 1; i <= n; i++) {
+            Perfume p = perfumes.get(i);
+            if(p.removed) continue;
+            for(int v = p.power; v <= k; v++) {
+                if(dp[v - p.power] == Integer.MAX_VALUE) continue;      // 만들 수 있는 방법 X
+
+                if(dp[v] > dp[v - p.power] + 1) {
+                    dp[v] = dp[v - p.power] + 1;
+                }
+            }
+        }
+        return dp[k] == Integer.MAX_VALUE ? -1 : dp[k];
+    }
+    static long query2() {
+        int k = Integer.parseInt(st.nextToken());
+        List<Perfume> availablePerfumes = new ArrayList<>();
+        int[] freq = new int[3001];     // 향료의 최대는 3000
+        for(int i = 1; i <= n; i++) {
+            if(perfumes.get(i).removed) continue;
+            availablePerfumes.add(perfumes.get(i));
+            freq[perfumes.get(i).power]++;
+        }
+        int size = availablePerfumes.size();
+        /**
+         * [i] : power 가 i 이상인 향료들의 누적 합
+         */
+        int[] suffix = new int[3002];
+        for(int i = 3000; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + freq[i];
+        }
+
+        long count = 0;
+        for(int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++) {
+                int sum = availablePerfumes.get(i).power +
+                        availablePerfumes.get(j).power;
+
+                // k 이상이 되기까지 남은 power
+                int diff = k - sum;
+                if(diff <= 0) {     // 어떤 것을 선택해도 가능
+                    count += suffix[0];
+                } else if(diff <= 3000) {
+                    count += suffix[diff];
+                }
+            }
+        }
+        return count;
+    }
+}
