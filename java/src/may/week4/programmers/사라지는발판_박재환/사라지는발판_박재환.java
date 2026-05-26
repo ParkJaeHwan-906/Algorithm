9bcd0ece4a3601af487a3dd3714155066383408e
Hwannee00 <doormoo2@naver.com>
2026-05-26 14:09:31 +0900

    chore: Programmers 문제 Package로 분리

---
+++ java/src/may/week4/programmers/사라지는발판_박재환/사라지는발판_박재환.java
@@ -0,0 +1,19 @@
+package may.week4.programmers.사라지는발판_박재환;
+
+public class 사라지는발판_박재환 {
+    public static void main(String[] args) {
+        int[][] board = {};
+        int[] aloc = {};
+        int[] bloc = {};
+
+        Solution solution = new Solution();
+        System.out.println(solution.solution(board, aloc, bloc));
+    }
+}
+
+class Solution {
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        int answer = -1;
+        return answer;
+    }
+}
