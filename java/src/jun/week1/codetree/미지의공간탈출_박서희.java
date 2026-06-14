9cd26ed92ab9916b68a504e416d482a36a3d5884
seohee-P <p03061180@gmail.com>
2026-06-15 00:28:27 +0900

    CODETREE 미지의 공간 탈출 풀이 - 박서희

--- java/src/jun/week1/codetree/미지의공간탈출_박서희.java
+++ java/src/jun/week1/codetree/미지의공간탈출_박서희.java
@@ -3,10 +3,14 @@ package jun.week1.codetree;
 import java.io.*;
 import java.util.*;
 
-
+/*
+  문제풀이 시간: 이틀..?
+  AI 사용 여부: OOO AI 없으면 못 풀었다..일단 3차원끼리 이동 매핑도 눈 빠지는줄 알았다.. 그리고 turn 마다 이상현상 일으키는 부분도 어려워서 도움을 받았다.
+  이상 현상이 시간의 벽을 타고 확산되는 과정이 있을수도 생각해서 아주 복잡하게 생각했는데 토론 탭에서 그 부분은 생각하지 않아도 된다고 한다.
+  시간의 벽에서 평면으로 내려올 수 있는 출구가 하나라 여기가 막히면 영영 탈출할 수 없어서 평면 상에서 확산만 생각하면 된다고 한다.
+ */
 public class 미지의공간탈출_박서희 {
     static final int EAST = 0, WEST = 1, SOUTH = 2, NORTH = 3, TOP = 4, BOTTOM = 5;
-    // 동 서 남 북
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
 
@@ -39,7 +43,6 @@ public class 미지의공간탈출_박서희 {
             }
         }
 
-        // 동 서 남 북 윗면
         grid3D = new int[5][M][M];
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < M; j++) {
@@ -60,22 +63,20 @@ public class 미지의공간탈출_박서희 {
             }
             temp[4] = BOTTOM;
             strange.add(temp);
+            grid[temp[0]][temp[1]] = 9;
         }
 
         bfs();
-
     }
 
     static int[] move3D(int currentBoard, int r, int c, int dir) {
         int nextR = r + dx[dir];
         int nextC = c + dy[dir];
 
-        // 경우 1: 여전히 같은 면 내부에 있는 경우
         if (inRange(nextR, nextC, M, M)) {
             return new int[]{currentBoard, nextR, nextC};
         }
 
-        // 경우 2: 범위를 벗어나서 다른 면으로 넘어가는 경우
         switch (currentBoard) {
             case TOP:
                 if (dir == NORTH) return new int[]{NORTH, 0, M - 1 - c};
@@ -107,7 +108,7 @@ public class 미지의공간탈출_박서희 {
 
             case NORTH:
                 if (dir == NORTH) return new int[]{TOP, 0, M - 1 - c};
-                if (dir == SOUTH) return new int[]{BOTTOM, startGrid3D[0], startGrid3D[1] + M - 1 - c};
+                if (dir == SOUTH) return new int[]{BOTTOM, startGrid3D[0] - 1, startGrid3D[1] + M - 1 - c};
                 if (dir == WEST) return new int[]{EAST, r, M - 1};
                 if (dir == EAST) return new int[]{WEST, r, 0};
                 break;
@@ -116,7 +117,6 @@ public class 미지의공간탈출_박서희 {
         return null;
     }
 
-    // 시간 이상 현상
     static void moveStrange(List<int[]> strange, int turn) {
         for (int[] s : strange) {
             int r = s[0];
@@ -126,54 +126,43 @@ public class 미지의공간탈출_박서희 {
             int board = s[4];
 
             if (turn % v == 0) {
-
-                // 경우 1: 시간 이상 현상이 바닥 평면에 있을 때
                 if (board == BOTTOM) {
                     int nextR = r + dx[dir];
                     int nextC = c + dy[dir];
 
                     if (!inRange(nextR, nextC, N, N)) continue;
+                    if (grid[nextR][nextC] == 4) continue;
 
-                    // 서쪽 3d 차원
                     if (dir == EAST && nextC == startGrid3D[1] && nextR >= startGrid3D[0] && nextR < startGrid3D[0] + M) {
                         s[4] = WEST;
                         s[0] = M - 1;
                         s[1] = nextR - startGrid3D[0];
                         grid3D[s[4]][s[0]][s[1]] = 9;
-                    }
-                    // 남쪽 3d 차원
-                    else if (dir == NORTH && nextR == startGrid3D[0] + M - 1 && nextC >= startGrid3D[1] && nextC < startGrid3D[1] + M) {
+                    } else if (dir == NORTH && nextR == startGrid3D[0] + M - 1 && nextC >= startGrid3D[1] && nextC < startGrid3D[1] + M) {
                         s[4] = SOUTH;
                         s[0] = M - 1;
                         s[1] = nextC - startGrid3D[1];
                         grid3D[s[4]][s[0]][s[1]] = 9;
-                    }
-                    // 북쪽 3d 차원
-                    else if (dir == SOUTH && nextR == startGrid3D[0] && nextC >= startGrid3D[1] && nextC < startGrid3D[1] + M) {
+                    } else if (dir == SOUTH && nextR == startGrid3D[0] && nextC >= startGrid3D[1] && nextC < startGrid3D[1] + M) {
                         s[4] = NORTH;
                         s[0] = M - 1;
-                        s[1] = nextC - startGrid3D[1];
+                        s[1] = startGrid3D[1] + M - 1 - nextC;
                         grid3D[s[4]][s[0]][s[1]] = 9;
-                    }
-                    // 동쪽 3d 차원
-                    else if (dir == WEST && nextC == startGrid3D[1] + M - 1 && nextR >= startGrid3D[0] && nextR < startGrid3D[0] + M) {
-                        s[4] = EAST; // 동쪽 면으로 진입
-                        s[0] = M - 1; // 가장 아래쪽 행
+                    } else if (dir == WEST && nextC == startGrid3D[1] + M - 1 && nextR >= startGrid3D[0] && nextR < startGrid3D[0] + M) {
+                        s[4] = EAST;
+                        s[0] = M - 1;
                         s[1] = startGrid3D[0] + M - 1 - nextR;
                         grid3D[s[4]][s[0]][s[1]] = 9;
                     } else {
                         s[0] = nextR;
                         s[1] = nextC;
-                        grid[s[0]][s[1]] = 9; // 바닥 평면에 장애물 표시
+                        grid[s[0]][s[1]] = 9;
                     }
-                }
-
-                // 경우 2: 시간 이상 현상이 이미 주사위 면 위에 있을 때
-                else {
-
+                } else {
                     int[] next = move3D(board, r, c, dir);
 
                     if (next != null) {
+                        if (next[0] == BOTTOM && grid[next[1]][next[2]] == 4) continue;
                         s[4] = next[0];
                         s[0] = next[1];
                         s[1] = next[2];
@@ -192,14 +181,11 @@ public class 미지의공간탈출_박서희 {
     static void bfs() {
         Queue<int[]> q = new LinkedList<>();
 
-
         boolean[][][] visited3D = new boolean[5][M][M];
         boolean[][] visitedGrid = new boolean[N][N];
 
-
         q.add(new int[]{tm.board, tm.r, tm.c, 0});
         visited3D[tm.board][tm.r][tm.c] = true;
-
 
         int lastUpdatedTime = -1;
 
@@ -210,27 +196,22 @@ public class 미지의공간탈출_박서희 {
             int cc = curr[2];
             int cTime = curr[3];
 
-
             if (cb == BOTTOM && cr == exit[0] && cc == exit[1]) {
                 System.out.println(cTime);
                 return;
             }
 
-            // 새로운 시간대로 진입할 때만 시간 이상 현상을 1칸 확산시킴
             if (lastUpdatedTime < cTime) {
                 moveStrange(strange, cTime + 1);
                 lastUpdatedTime = cTime;
             }
 
-
             for (int i = 0; i < 4; i++) {
-
                 if (cb == BOTTOM) {
                     int nr = cr + dx[i];
                     int nc = cc + dy[i];
 
                     if (!inRange(nr, nc, N, N)) continue;
-
 
                     if (i == EAST && nc == startGrid3D[1] && nr >= startGrid3D[0] && nr < startGrid3D[0] + M) {
                         int nb = WEST;
@@ -248,19 +229,15 @@ public class 미지의공간탈출_박서희 {
                             visited3D[nb][nr3][nc3] = true;
                             q.add(new int[]{nb, nr3, nc3, cTime + 1});
                         }
-                    }
-                    // 북쪽 벽 진입
-                    else if (i == SOUTH && nr == startGrid3D[0] && nc >= startGrid3D[1] && nc < startGrid3D[1] + M) {
+                    } else if (i == SOUTH && nr == startGrid3D[0] && nc >= startGrid3D[1] && nc < startGrid3D[1] + M) {
                         int nb = NORTH;
                         int nr3 = M - 1;
-                        int nc3 = nc - startGrid3D[1];
+                        int nc3 = M - 1 - nc + startGrid3D[1];
                         if (!visited3D[nb][nr3][nc3] && grid3D[nb][nr3][nc3] != 1 && grid3D[nb][nr3][nc3] != 9) {
                             visited3D[nb][nr3][nc3] = true;
                             q.add(new int[]{nb, nr3, nc3, cTime + 1});
                         }
-                    }
-                    // 동쪽 벽 진입
-                    else if (i == WEST && nc == startGrid3D[1] + M - 1 && nr >= startGrid3D[0] && nr < startGrid3D[0] + M) {
+                    } else if (i == WEST && nc == startGrid3D[1] + M - 1 && nr >= startGrid3D[0] && nr < startGrid3D[0] + M) {
                         int nb = EAST;
                         int nr3 = M - 1;
                         int nc3 = startGrid3D[0] + M - 1 - nr;
@@ -268,19 +245,13 @@ public class 미지의공간탈출_박서희 {
                             visited3D[nb][nr3][nc3] = true;
                             q.add(new int[]{nb, nr3, nc3, cTime + 1});
                         }
-                    }
-
-                    // 평범한 바닥 이동 (주사위 본체 구역인 3은 디디면 안 됨)
-                    else {
+                    } else {
                         if (grid[nr][nc] != 1 && grid[nr][nc] != 9 && grid[nr][nc] != 3 && !visitedGrid[nr][nc]) {
                             visitedGrid[nr][nc] = true;
                             q.add(new int[]{BOTTOM, nr, nc, cTime + 1});
                         }
                     }
-                }
-
-                // 2) 현재 플레이어가 3D 주사위 면 위에 있을 때
-                else {
+                } else {
                     int[] next = move3D(cb, cr, cc, i);
                     if (next == null) continue;
 
@@ -288,12 +259,12 @@ public class 미지의공간탈출_박서희 {
                     int nr = next[1];
                     int nc = next[2];
 
-                    if (nb == BOTTOM) { // 주사위에서 바닥으로 튕겨 내려온 경우
+                    if (nb == BOTTOM) {
                         if (grid[nr][nc] != 1 && grid[nr][nc] != 9 && !visitedGrid[nr][nc]) {
                             visitedGrid[nr][nc] = true;
                             q.add(new int[]{nb, nr, nc, cTime + 1});
                         }
-                    } else { // 계속 주사위 다른 면으로 이동하는 경우
+                    } else {
                         if (grid3D[nb][nr][nc] != 1 && grid3D[nb][nr][nc] != 9 && !visited3D[nb][nr][nc]) {
                             visited3D[nb][nr][nc] = true;
                             q.add(new int[]{nb, nr, nc, cTime + 1});
@@ -302,7 +273,6 @@ public class 미지의공간탈출_박서희 {
                 }
             }
         }
-        // 탈출 불가능
         System.out.println(-1);
     }
 
