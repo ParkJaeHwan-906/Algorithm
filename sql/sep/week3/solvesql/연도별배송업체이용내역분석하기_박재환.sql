e7a24a8c62a0d2cdc4c2464921ac5d7c0532b0b4
Hwannee00 <doormoo2@naver.com>
2026-09-26 07:40:56 +0900

    SOLVESQL 연도별 배송 업체 이용 내역 분석하기 풀이 - 박재환

---
+++ sql/sep/week3/solvesql/연도별배송업체이용내역분석하기_박재환.sql
@@ -0,0 +1,7 @@
+SELECT
+    YEAR (purchased_at) AS year, SUM (CASE WHEN shipping_method = 'Standard' THEN 1 ELSE 0 END)
+    + SUM (CASE WHEN is_returned = 1 THEN 1 ELSE 0 END) AS standard, SUM (CASE WHEN shipping_method = 'Express' THEN 1 ELSE 0 END) AS express, SUM (CASE WHEN shipping_method = 'Overnight' THEN 1 ELSE 0 END) AS overnight
+FROM transactions
+WHERE is_online_order = 1
+GROUP BY YEAR (purchased_at)
+ORDER BY year;
